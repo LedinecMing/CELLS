4b595f412c08937a225f55a15a5114e476315757
LedinecMing <63245550+LedinecMing@users.noreply.github.com>
2021-01-12 21:14:39 +0400

    Update CELLS.cxx
    Yep update with graphic 🥳

--- CELLS.cxx
+++ CELLS.cxx
@@ -1,190 +1,218 @@
-#include <iostream>  
-#include <string>  
+#include <iostream>
+#include <string>
 #include <ctime>
 #include <stdlib.h>
 #include <cstdlib>
 #include <SDL2/SDL.h>
 
-using namespace std; 
-const int code_size=8;
-const int size=20;
-const int sun=900;
-const int commands_count=8;
-const int max_command=8;
-int mod(int x,int N){
-    return (x % N + N) %N;
+using namespace std;
+const int code_size = 8;
+const int size = 20;
+const int sun = 900;
+const int commands_count = 8;
+int mod(int x, int N)
+{
+    return (x % N + N) % N;
 }
 class Point
 {
-    
-    public:
-        Point()
-            {
-            }
-        Point(int newX, int newY,  int types)
-            {
-                this->x=newX;
-                this->x=newY;
-                this->type=types;
-            }
-         void setPos(int newX,int newY)
-             {
-                this->x=newX;
-                this->y=newY;
-             }
-         void setCode(int newCode[code_size])
-         {
-             for( int i=0; i<code_size+1; i++)
-                 {
-                     this->code[i]=newCode[i];
-                 }
-         }
-         int getX()
-             {
-                return x;
-             }
-         int getY()
-             {
-                 return y;
-             }
-         // Метод деления клетки
-         void Divide(Point world[size][size])
-             {
-                 // У рождаемой клетки будет половина энергии родителя
-                 int babyEnergy=energy/2;
-                 
-                 int plusX,plusY;
-                 // Тут циклично выборка места рождения с помощью рандома
-                 tuda:
-                 plusX=-1+(rand()%3);
-                 plusY=-1+(rand()%3);
-                 if(plusX==0 and plusY==0)
-                 {
-                     goto tuda;
-                     // Если получаемые значения - нули переходим tuda :D
-                 }                                 
-                 // Новый код это копия кода родителя
-                 int newCode[code_size];
-                 for(int i=0;i<code_size;i++)
-                     {
-                             newCode[i]=code[i];
-                     }
-                 // Мутация с шансом 20% в коде
-                if(rand()%6==5)
-                {
-                     newCode[rand()%(code_size+1)]=rand()%commands_count;
-                 }
-                 // Устанавливаем все параметры клетки
-                 
-                 Point *place=&world[(mod(x+plusX,size+1))][mod((y+plusY),(size+1))];
-                 if((*place).type==0)
-                 {
-                 this->energy/=2;
-                 (*place).type=type;
-                 (*place).setCode(newCode);
-                 (*place).energy=babyEnergy;
-                 }
-                 
-             }
-        void setCounter(int newCounter)
+  public:
+    Point()
+    {
+    }
+    Point(int newX, int newY, int types)
+    {
+        this->x = newX;
+        this->x = newY;
+        this->type = types;
+    }
+    void setPos(int newX, int newY)
+    {
+        this->x = newX;
+        this->y = newY;
+    }
+    void setCode(int newCode[code_size])
+    {
+        for (int i = 0; i < code_size + 1; i++)
         {
-            this->counter=newCounter;
+            this->code[i] = newCode[i];
         }
-        int cycle(Point world[size][size])
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+    // Метод деления клетки
+    void Divide(Point world[size][size])
+    {
+        // У рождаемой клетки будет половина энергии родителя
+        int babyEnergy = energy / 2;
+
+        int plusX, plusY;
+    // Тут циклично выборка места рождения с помощью рандома
+    tuda:
+        plusX = -1 + (rand() % 3);
+        plusY = -1 + (rand() % 3);
+        if (plusX == 0 and plusY == 0)
         {
-            
-            int i=code[counter];
-            this->energy-=10;
-            if(energy<200)
+            goto tuda;
+            // Если получаемые значения - нули переходим tuda :D
+        }
+        // Новый код это копия кода родителя
+        int newCode[code_size];
+        for (int i = 0; i < code_size; i++)
+        {
+            newCode[i] = code[i];
+        }
+        // Мутация с шансом 20% в коде
+        if (rand() % 6 == 5)
+        {
+            newCode[rand() % (code_size + 1)] = rand() % commands_count;
+        }
+        // Устанавливаем все параметры клетки
+
+        Point *place = &world[(mod(x + plusX, size + 1))][mod((y + plusY), (size + 1))];
+        if ((*place).type == 0)
+        {
+            this->energy /= 2;
+            (*place).type = type;
+            (*place).setCode(newCode);
+            (*place).energy = babyEnergy;
+        }
+    }
+    void setCounter(int newCounter)
+    {
+        this->counter = newCounter;
+    }
+    int cycle(Point world[size][size])
+    {
+        int i = code[counter];
+        this->energy -= 10;
+        if (energy < 200)
+        {
+            world[x][y].type = 0;
+            world[x][y].energy = 0;
+            world[x][y].counter = 0;
+        }
+        if (energy > 1000)
+        {
+            Divide(world);
+        }
+        if (i == 0)
+        {
+            this->counter = mod(counter + 1, code_size);
+            this->energy += sun / size * (size - y);
+            return 1;
+        }
+        if (i < 9 and i > 0)
+        {
+            // Передвижение по поверхности тора
+            int moves[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+            this->counter = mod(counter + 1, code_size);
+            int newX = mod((x + moves[i - 1][0]), size + 1);
+            int newY = mod((y + moves[i - 1][1]), size + 1);
+            Point place = world[newX][newY];
+            if (place.type == 0)
             {
-                world[x][y].type=0;
-                world[x][y].energy=0;
-                world[x][y].counter=0;
+                place.type = type;
+                place.energy = energy;
+                place.setCounter(counter);
+                place.setCode(code);
+                this->type = 0;
+                this->energy = 0;
             }
-            if(energy>1000)
-            {
-                Divide(world);
-            }
-            if(i==0)
-            {
-               
-                this->counter=mod(counter+1,code_size);
-                this->energy+=sun/size*(size-y);
-                return 1;
-            }
-            if(i<9 and i>0)
-            {
-                // Передвижение по поверхности тора
-                int moves[8][2]={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
-                this->counter=mod(counter+1,code_size);
-                int newX=mod((x+moves[i-1][0]),size+1);
-                int newY=mod((y+moves[i-1][1]),size+1);
-                Point place=world[newX][newY];
-                if(place.type==0)
-                {
-                    place.type=type;
-                    place.energy=energy;
-                    place.setCounter(counter);
-                    place.setCode(code);
-                    this->type=0;
-                    this->energy=0;
-                }
-                return 1;
-            }
+
+            return 1;
+        }
         return 0;
-        }
-    public:
-        int type;
-        int energy;
-        int code[code_size];
-    private:
-        int x;
-        int y;
-        int counter=0;
+    }
+
+  public:
+    int type;
+    int energy;
+    int code[code_size];
+
+  private:
+    int x;
+    int y;
+    int counter = 0;
+    bool killed = false;
 };
-int main()  
-{  
-srand(time(nullptr));
-
-Point world[size][size];
-for(int i=0; i<size; i++){
-    for(int j=0; j<size; j++){
-        world[i][j].setPos(i,j);
-        world[i][j].type=0;
-                                       }
-                            }
-world[0][0].type=1;
-world[0][0].energy=250;
-int code[code_size];
-for(int i=0;i<code_size+1;i++)
+int main()
 {
+    srand(time(nullptr));
 
-    code[i]=0;
-}
-world[0][0].setCode(code);
-for(int cycle=0;;cycle++)
-{
-    system("clear");
-    for(int i=0;i<size;i++)
+    Point world[size][size];
+    for (int i = 0; i < size; i++)
     {
-        for(int j=0;j<size;j++)
+        for (int j = 0; j < size; j++)
         {
-            //cout<<i<<" "<<j<<endl;
-            if(world[i][j].type!=0)
+            world[i][j].setPos(i, j);
+            world[i][j].type = 0;
+        }
+    }
+    world[0][0].type = 1;
+    world[0][0].energy = 250;
+    int code[code_size];
+    for (int i = 0; i < code_size + 1; i++)
+    {
+        code[i] = 0;
+    }
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Window *window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    if (window == NULL)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xAFF, 0xAFF, 0xAFF));
+    SDL_UpdateWindowSurface(window);
+    world[0][0].setCode(code);
+    world[0][0].type = 1;
+    for (int cycle = 0;; cycle++)
+    {
+        system("clear");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
             {
-            world[i][j].cycle(world);
+                //cout<<i<<" "<<j<<endl;
+                if (world[i][j].type != 0)
+                {
+                    world[i][j].cycle(world);
+                }
             }
         }
-    }
-    for(int i=0;i<size;i++)
-    {
-        for(int j=0;j<size;j++)
+        for (int i = 0; i < size; i++)
         {
-            cout<<world[i][j].type;
+            for (int j = 0; j < size; j++)
+            {
+                if(not world[i][j].type)
+                {
+                    continue;
+                }
+                SDL_Rect rect;
+                rect.x=i;
+                rect.y=j;
+                rect.h=0;
+                rect.w=0;
+                //const SDL_Rect *silka=rect;
+                SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format,0xFF,world[i][j].energy,0xFF));
+            }
+            cout << endl;
         }
-        cout<<endl;
+
+        SDL_UpdateWindowSurface(window);
     }
+
+    return 0;
 }
-    
-return 0;
-}  
